d4dcfa900bdd5a53806ffe762893c02d6a3480c9
Rene Rubalcava <odoe@odoe.net>
2019-06-17 18:36:07 -0700

    add script to update dojorc

---
+++ scripts/prebuild.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const fs = require('fs-extra');
+const directoryPath = path.join(__dirname, '../posts');
+const dojorcPath = path.join(__dirname, '../.dojorc');
+
+// read dojorc first
+fs.readJson(dojorcPath, (error, dojorc) => {
+	if (error) throw error;
+
+	// read the posts directory
+	fs.readdir(directoryPath, function(err, files) {
+		//handling error
+		if (err) {
+			return console.log('Unable to scan directory: ' + err);
+		}
+
+		const posts = files.sort().map(function(file) {
+			// remove the file extension
+			return file.replace('.md', '');
+		});
+
+		dojorc['build-app']['build-time-render'].paths = posts;
+		dojorc['build-app']['build-time-render'].paths.push('.');
+		fs.writeJson(dojorcPath, dojorc, { spaces: 2 }, (err) => {
+			if (err) throw err;
+			console.log('.dojorc updated');
+		});
+	});
+});
