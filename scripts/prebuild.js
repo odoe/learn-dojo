402027c98a9ffcad741faac089e6a5bae5349e2f
odoe <rrubalcava@esri.com>
2019-08-28 07:47:00 -0700

    add dev and prod dojorc configs

--- scripts/prebuild.js
+++ scripts/prebuild.js
@@ -1,35 +1,44 @@
 const path = require('path');
 const fs = require('fs-extra');
 const directoryPath = path.join(__dirname, '../posts');
-const dojorcPath = path.join(__dirname, '../.dojorc');
+const devDojorcPath = path.join(__dirname, '../dev.dojorc');
+const prodDojorcPath = path.join(__dirname, '../prod.dojorc');
 
-// read dojorc first
-fs.readJson(dojorcPath, (error, dojorc) => {
-	if (error) throw error;
+updateDojoRcFiles([devDojorcPath, prodDojorcPath]);
 
-	// read the posts directory
-	fs.readdir(directoryPath, function(err, files) {
-		//handling error
-		if (err) {
-			return console.log('Unable to scan directory: ' + err);
-		}
+function updateDojoRcFiles(paths) {
+	for (let i = 0; i < paths.length; i++) {
+		const dojorcPath = paths[i];
 
-		const posts = files
-			.filter(function(file) {
-				// don't include the template
-				return file !== `template.md`;
-			})
-			.sort()
-			.map(function(file) {
-				// remove the file extension
-				return file.replace('.md', '');
+		// read dojorc first
+		fs.readJson(dojorcPath, (error, dojorc) => {
+			if (error) throw error;
+
+			// read the posts directory
+			fs.readdir(directoryPath, function(err, files) {
+				//handling error
+				if (err) {
+					return console.log('Unable to scan directory: ' + err);
+				}
+
+				const posts = files
+					.filter(function(file) {
+						// don't include the template
+						return file !== `template.md`;
+					})
+					.sort()
+					.map(function(file) {
+						// remove the file extension
+						return file.replace('.md', '');
+					});
+
+				dojorc['build-app']['build-time-render'].paths = posts;
+				dojorc['build-app']['build-time-render'].paths.push('.');
+				fs.writeJson(dojorcPath, dojorc, { spaces: 2 }, (err) => {
+					if (err) throw err;
+					console.log('.dojorc updated');
+				});
 			});
-
-		dojorc['build-app']['build-time-render'].paths = posts;
-		dojorc['build-app']['build-time-render'].paths.push('.');
-		fs.writeJson(dojorcPath, dojorc, { spaces: 2 }, (err) => {
-			if (err) throw err;
-			console.log('.dojorc updated');
 		});
-	});
-});
+	}
+}
