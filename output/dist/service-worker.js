1d18869a8fa8e0d8d7a7d409755fac3bac59764d
odoe <rrubalcava@esri.com>
2019-10-03 07:14:39 -0700

    update post, build fixes, should be faster

--- output/dist/service-worker.js
+++ output/dist/service-worker.js
@@ -15,7 +15,7 @@ importScripts("workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "workbox-v3.6.3"});
 
 importScripts(
-  "precache-manifest.0ee5062a8377f1ced1b2722b618ceb9e.js"
+  "precache-manifest.38aacfff56fb53d629940da5c9c05829.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "learn-dojo"});
