e10c1c9772a9bac08cd7aee05233c69be107a95b
odoe <rrubalcava@esri.com>
2019-12-18 07:04:34 -0800

    resize hero image

--- output/dist/service-worker.js
+++ output/dist/service-worker.js
@@ -15,7 +15,7 @@ importScripts("workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "workbox-v3.6.3"});
 
 importScripts(
-  "precache-manifest.38a1289ee4cac515407470d243774cad.js"
+  "precache-manifest.e3682fee079e4d53f1da6adcaa72f452.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "learn-dojo"});
